e5facd03ed03ce1eb30504b56b6a9b97632dc7fe
Christian Stigen <Christian.Stigen@Emerson.com>
2015-02-13 08:55:54 +0100

    Scores higher for shorter genetic programs, adds insertion and deletion mutations

--- genetic.py
+++ genetic.py
@@ -7,6 +7,8 @@ TODO:
         parent2:    ---------
         result:     ..----...
         instead of: ....-----
+- Refactor the main code, make it modular (should be completely controllable
+  from the outside)
 """
 
 import random
@@ -18,16 +20,17 @@ class GeneticMachine(vm.Machine):
     def __init__(self, code=[]):
         vm.Machine.__init__(self, code)
 
-    def randomize(self, length=10, maxabsint=9999, maxstrlen=10):
+    def randomize(self, min_code_length=10, max_code_length=10, maxabsint=9999,
+            maxstrlen=10, excluded_ops=[".", "exit", "read", "write", "stack"]):
         """Creates random instructions."""
         self.code = []
 
+        # Remove some commands we don't wand the machine to include
         ops = self.dispatch_map
-        del ops["."]
-        del ops["exit"]
-        del ops["read"]
-        del ops["write"]
-        del ops["stack"]
+        for op in excluded_ops:
+            del ops[op]
+
+        length = random.randint(min_code_length, max_code_length)
 
         for _ in xrange(length):
             optype = random.randint(0,100)
@@ -73,7 +76,8 @@ class GeneticMachine(vm.Machine):
         else:
             return None
 
-def main(num_machines=1000, generations=100000, steps=20, codelen=10,
+# TODO:
+def main(num_machines=1000, generations=100000, steps=20, max_codelen=10,
         keep_top=100, mutation_rate=0.05):
 
     """Attemptes to create a program that puts the number 123 on the top of the
@@ -82,22 +86,25 @@ def main(num_machines=1000, generations=100000, steps=20, codelen=10,
 
     print("Using GP to create a program that puts 123 on the ToS.")
 
-    def fitfunc(res):
-        if isinstance(res, int):
-            return abs(123.0 - res)/123.0
-        else:
-            return 9999.9
+    def fitfunc(vm):
+        # Machines with no code should not live to next generation
+        if len(vm.stack) > 0 and len(vm.code) > 0:
+            tos = vm.top
+            if isinstance(tos, int):
+                distance = abs(123.0 - tos)/123.0
+                return distance, len(vm.code)
+        return 9999.9, len(vm.code)
 
-    machines = [GeneticMachine().randomize(codelen) for n in xrange(0,num_machines)]
+    machines = [GeneticMachine().randomize(1, max_codelen) for n in xrange(0,num_machines)]
 
     try:
         for no in xrange(0, generations):
             # Run all machines and collect results (top of stack)
             results = [(m.reset().run_limited(steps), i) for (i,m) in enumerate(machines)]
 
-            # Calculate their fitness score
+            # Calculate their fitness score, sort by fitness, code length
             orig = machines
-            fitness = sorted(map(lambda (r,i): (fitfunc(r),i), results))
+            fitness = sorted(map(lambda (r,i): (fitfunc(orig[i]),i), results))
 
             # Select the best
             best = [machines[i] for (r,i) in fitness[:keep_top]]
@@ -107,6 +114,7 @@ def main(num_machines=1000, generations=100000, steps=20, codelen=10,
             machines = []
             while len(machines) < num_machines:
                 try:
+                    # TODO: Could make sure that m!=f here
                     m = random.choice(best)
                     f = random.choice(best)
                     machines.append(m.crossover(f))
@@ -116,19 +124,38 @@ def main(num_machines=1000, generations=100000, steps=20, codelen=10,
             # Adds mutations from time to time
             for m in machines:
                 if random.random() < mutation_rate:
+                    # Mutation consists of changing, inserting or deleting an
+                    # instruction
+                    kind = random.random()
                     i = random.randint(0, -1+len(m.code))
-                    m.code[i] = GeneticMachine([]).randomize().code[0]
+                    op = GeneticMachine([]).randomize().code[0]
+                    if kind <= 0.5:
+                        # change
+                        m.code[i] = op
+                    elif kind <= 0.75:
+                        # deletion
+                        del m.code[i]
+                    else:
+                        # insertion
+                        m.code.insert(i, op)
 
             # Display results
             chosen = [(orig[i], result) for result, i in fitness[:keep_top]]
-            avg = sum(res for m,res in chosen)/len(chosen)
-            print("Gen %d: avg %s" % (no, avg))
+            avg = sum(res for (m,(res,codelen)) in chosen)/float(len(chosen))
+            avglen = sum(codelen for (m,(res,codelen)) in chosen)/float(len(chosen))
+            print("Gen %d: machines=%d avg_fitness=%.7f avg_codelen=%.7f" %
+                    (no, len(orig), avg, avglen))
+
+            if avg == 0.0 and avglen < 2:
+                print("Stopping because avg==0 and avglen<2.")
+                break
     except KeyboardInterrupt:
         pass
 
     print("Best 10:")
-    for m, result in chosen[0:10]:
-        print("fitness=%f tos=%s code: %s" % (result, m.top, m.code_string))
+    for m, (result, codelen) in chosen[0:10]:
+        print("fitness=%f tos=%s len=%d code: %s" % (result, m.top,
+            len(m.code), m.code_string))
 
 if __name__ == "__main__":
     main()
