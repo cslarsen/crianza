33da9cd90040a28901e2e825b70dbae751485c49
Christian Stigen Larsen <csl@sublevel3.org>
2015-02-14 18:03:27 +0100

    Refactors genetic machine

--- genetic.py
+++ genetic.py
@@ -16,82 +16,74 @@ TODO:
 import random
 import vm
 
-class GeneticMachine(vm.Machine):
-    """A Machine suited for genetic programming."""
-    def __init__(self, code=[]):
-        super(GeneticMachine, self).__init__(code)
+def randomize(vm,
+        length=(10,10),
+        ints=(0,999),
+        strs=(1,10),
+        instruction_ratio=0.5,
+        number_string_ratio=0.8,
+        exclude=[".", "exit", "read", "write", "stack"]):
 
-    def randomize(self,
-            length=(10,10),
-            ints=(0,999),
-            strs=(1,10),
-            instruction_ratio=0.5,
-            number_string_ratio=0.8,
-            exclude=[".", "exit", "read", "write", "stack"]):
+    """Replaces existing code with completely random instructions. Does not
+    optimize code after generating it.
 
-        """Replaces existing code with completely random instructions. Does not
-        optimize code after generating it.
+    Args:
+        length: Tuple of minimum and maximum code lengths. Code length will
+        be a random number between these two, inclusive values.
 
-        Args:
-            length: Tuple of minimum and maximum code lengths. Code length will
-            be a random number between these two, inclusive values.
+        ints: Integers in the code will be selected at random from this
+        inclusive range.
 
-            ints: Integers in the code will be selected at random from this
-            inclusive range.
+        strs: Inclusive range of the length of strings in the code.
 
-            strs: Inclusive range of the length of strings in the code.
+        instruction_ratio: Ratio of instructions to numbers/strings,
+        meaning that if this value is 0.5 then there will just as many
+        instructions in the code as there are numbers and strings.
 
-            instruction_ratio: Ratio of instructions to numbers/strings,
-            meaning that if this value is 0.5 then there will just as many
-            instructions in the code as there are numbers and strings.
+        number_string_ratio: Ratio of numbers to strings.
 
-            number_string_ratio: Ratio of numbers to strings.
+        exclude: Excluded instructions. For genetic programming, one wants
+        to avoid the program to hang for user input.  The default value is
+        to exclude console i/o and debug instructions.
 
-            exclude: Excluded instructions. For genetic programming, one wants
-            to avoid the program to hang for user input.  The default value is
-            to exclude console i/o and debug instructions.
+    Returns:
+        The VM.
+    """
+    vm.code = []
+    instructions = dict([i for i in vm.instructions.items() if i[0] not
+        in exclude])
 
-        Returns:
-            The GeneticMachine.
-        """
-        self.code = []
-        instructions = dict([i for i in self.instructions.items() if i[0] not
-            in exclude])
+    for _ in xrange(random.randint(*length)):
+        r = random.random()
+        if r <= instruction_ratio:
+            # Generate a random instruction
+            vm.code.append(random.choice(instructions.keys()))
+        elif r <= number_string_ratio:
+            # Generate a random number
+            vm.code.append(random.randint(*ints))
+        else:
+            # Generate a random string
+            vm.code.append('"%s"' % "".join(chr(random.randint(1,127))
+                for n in xrange(0, random.randint(*strs))))
+    return vm
 
-        for _ in xrange(random.randint(*length)):
-            r = random.random()
-            if r <= instruction_ratio:
-                # Generate a random instruction
-                self.code.append(random.choice(instructions.keys()))
-            elif r <= number_string_ratio:
-                # Generate a random number
-                self.code.append(random.randint(*ints))
-            else:
-                # Generate a random string
-                self.code.append('"%s"' % "".join(chr(random.randint(1,127))
-                    for n in xrange(0, random.randint(*strs))))
-        return self
+def crossover(m, f):
+    """Produces an offspring from two Machines, whose code is a
+    combination of the two."""
+    point = random.randint(0, min(len(m.code), len(f.code)))
+    return vm.Machine(m.code[:point] + f.code[point:])
 
-    def crossover(self, other):
-        """Given another GeneticMachine, produces an offspring which is a
-        combination of the two's code.
-        """
-        point = random.randint(0, min(len(self.code), len(other.code)))
-        return GeneticMachine(self.code[:point] + other.code[point:])
 
-    def run(self, steps=100, propagate_errors=False):
-        try:
-            super(GeneticMachine, self).run(steps)
-        except StopIteration:
-            pass
-        except Exception:
-            if propagate_errors:
-                raise
-            # We don't want progarms with errors to live long, so return None
-            # here so that it will get a very low score.
-            return None
-
-        return None if len(self.stack)==0 else self.stack[-1]
+def run_result(vm, steps):
+    """Run a VM for a given number of steps and return True if it ran without
+    errors."""
+    try:
+        vm.run(steps)
+        return True
+    except StopIteration:
+        return True
+    except Exception:
+        return False
 
 
 def main(num_machines=1000, generations=100000, steps=20, max_codelen=10,
@@ -116,12 +108,14 @@ def main(num_machines=1000, generations=100000, steps=20, max_codelen=10,
                 return distance, slen + rlen, len(vm.code)
         return default
 
-    machines = [GeneticMachine().randomize(length=(1, max_codelen)) for n in xrange(0,num_machines)]
+    machines = [randomize(vm.Machine([]), length=(1, max_codelen)) for n in
+            xrange(0, num_machines)]
 
     try:
         for no in xrange(0, generations):
             # Run all machines and collect results (top of stack)
-            results = [(m.reset().run(steps), i) for (i,m) in enumerate(machines)]
+            results = [(run_result(m.reset(), steps), i) for (i,m) in
+                    enumerate(machines)]
 
             # Calculate their fitness score, sort by fitness, code length
             orig = machines
@@ -136,9 +130,8 @@ def main(num_machines=1000, generations=100000, steps=20, max_codelen=10,
             while len(machines) < num_machines:
                 try:
                     # TODO: Could make sure that m!=f here
-                    m = random.choice(best)
-                    f = random.choice(best)
-                    machines.append(m.crossover(f))
+                    machines.append(crossover(random.choice(best),
+                        random.choice(best)))
                 except IndexError:
                     break
 
@@ -157,7 +150,7 @@ def main(num_machines=1000, generations=100000, steps=20, max_codelen=10,
                         continue
                     if kind <= 0.5:
                         # change
-                        op = GeneticMachine([]).randomize().code[0]
+                        op = randomize(vm.Machine([])).code[0]
                         m.code[i] = op
                         mutations += 1
                     elif kind <= 0.75:
@@ -166,7 +159,7 @@ def main(num_machines=1000, generations=100000, steps=20, max_codelen=10,
                         mutations += 1
                     else:
                         # insertion
-                        op = GeneticMachine([]).randomize().code[0]
+                        op = randomize(vm.Machine([])).code[0]
                         m.code.insert(i, op)
                         mutations += 1
 
