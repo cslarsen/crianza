e9bf3291b73a0815ff75b5eadd2ae3a636aed33d
Christian Stigen Larsen <csl@sublevel3.org>
2015-02-14 11:49:19 +0100

    Adds more tests

--- test.py
+++ test.py
@@ -1,11 +1,26 @@
-from operator import *
+import StringIO
+import operator
 import unittest
+import vm
 
-from vm import Machine, constant_fold, MachineError, CompilationError, optimize
+fibonacci_source = \
+"""
+# The Fibonacci Sequence
+
+: println dup . ;
+: next swap over + ;
+
+# Start values
+0 println
+1 println
+
+# Loop forever
+@ next println return
+"""
 
 class TestVM(unittest.TestCase):
     def _Machine(self, *args):
-        return Machine(*args, output=None)
+        return vm.Machine(*args, output=None)
 
     def _run(self, *machine_args):
         """Runs machine and returns it."""
@@ -23,7 +38,7 @@ class TestVM(unittest.TestCase):
         self.assertEqual(self._run([1,2,3,4,5,"+","+","+","+"]).stack, [15])
 
     def test_arithmetic(self):
-        ops = [mul, add]
+        ops = [operator.mul, operator.add]
         for a in range(-10,10):
             for b in range(-10,10):
                 for op in ops:
@@ -31,21 +46,42 @@ class TestVM(unittest.TestCase):
 
     def test_optimizer_errors(self):
         for op in ["/", "mod", "div", "%"]:
-            with self.assertRaises(CompilationError):
-                constant_fold([2, 0, op], ignore_errors=False)
+            with self.assertRaises(vm.CompilationError):
+                vm.constant_fold([2, 0, op], ignore_errors=False)
 
     def test_optimizer(self):
-        self.assertEqual(constant_fold([2,3,"*","."]), [6,"."])
-        self.assertEqual(constant_fold([2,2,3,"*","."]), [2,6,"."])
-        self.assertEqual(constant_fold([5,2,3,"*","+","."]), [11,"."])
-        self.assertEqual(constant_fold([5,2,3,"*","+",4,"*","."]), [44,"."])
-        self.assertEqual(constant_fold([2, 3, "+", 5, "*", "println"]),
+        self.assertEqual(vm.constant_fold([2,3,"*","."]), [6,"."])
+        self.assertEqual(vm.constant_fold([2,2,3,"*","."]), [2,6,"."])
+        self.assertEqual(vm.constant_fold([5,2,3,"*","+","."]), [11,"."])
+        self.assertEqual(vm.constant_fold([5,2,3,"*","+",4,"*","."]), [44,"."])
+        self.assertEqual(vm.constant_fold([2, 3, "+", 5, "*", "println"]),
                 [25, "println"])
-        self.assertEqual(constant_fold([10, "dup"]), [10, 10])
-        self.assertEqual(constant_fold([1, 2, "dup", "dup", "+", "+"]), [1, 6])
-        self.assertEqual(constant_fold([1, 2, 3, "swap"]), [1, 3, 2])
-        self.assertEqual(constant_fold([1, 2, 3, "drop", "drop"]), [1])
+        self.assertEqual(vm.constant_fold([10, "dup"]), [10, 10])
+        self.assertEqual(vm.constant_fold([1, 2, "dup", "dup", "+", "+"]), [1, 6])
+        self.assertEqual(vm.constant_fold([1, 2, 3, "swap"]), [1, 3, 2])
+        self.assertEqual(vm.constant_fold([1, 2, 3, "drop", "drop"]), [1])
 
+    def test_program_fibonacci(self):
+        source = StringIO.StringIO(fibonacci_source)
+        code = vm.compile(vm.parse(source))
+        self.assertEqual(code, [0, 13, 'call', 1, 13, 'call', '@', 16, 'call',
+            13, 'call', 'return', 'exit', 'dup', '.', 'return', 'swap', 'over',
+            '+', 'return'])
+
+        machine = vm.Machine(code, output=None, optimize_code=False)
+
+        # skip to main loop
+        machine.run(11)
+
+        sequence = []
+        numbers_to_generate = 15
+
+        for its in xrange(0, numbers_to_generate):
+            sequence.append(machine.top)
+            machine.run(13) # next number
+
+        self.assertEqual(sequence, [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144,
+            233, 377, 610])
 
 if __name__ == "__main__":
     unittest.main()
