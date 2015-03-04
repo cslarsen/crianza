46a515964ab76419ae31a9afc748c5ccb37420b9
Christian Stigen <Christian.Stigen@Emerson.com>
2015-03-04 11:21:54 +0100

    Prepares PyPI package

--- setup.py
+++ setup.py
@@ -5,13 +5,17 @@ except ImportError:
 
 setup(
     name="crianza",
-    version="0.1",
-    author="Christian Stigen Larsen",
     packages=["crianza"],
-    scripts=["bin/crianza"],
+    version="0.1",
+    description="Simple Forth-like VM and genetic programming framework.",
+    author="Christian Stigen Larsen",
+    author_email="csl@csl.name",
     url="https://github.com/cslarsen/crianza",
+    download_url="https://github.com/cslarsen/crianza/tarball/0.1",
+    scripts=["bin/crianza"],
     license="LICENSE.txt",
-    description="Simple VM and genetic programming framework.",
     long_description=open("README.md").read(),
     zip_safe=True,
+    keywords=["vm", "virtual machine", "genetic programming", "interpreter"],
+    classifiers=[],
 )
